61ef89d36d87be31b3ab975df3b3a96756300f87
Jordan Knight <jakkaj@gmail.com>
2024-07-31 08:12:44 +1000

    HCI tries all devices

--- pkg/connector/ble/device_linux.go
+++ pkg/connector/ble/device_linux.go
@@ -1,14 +1,32 @@
 package ble
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/go-ble/ble"
 	"github.com/go-ble/ble/linux"
 )
 
 func newDevice() (ble.Device, error) {
-	device, err := linux.NewDevice()
-	if err != nil {
-		return nil, err
+	devices := []string{"hci0", "hci1", "hci2"} // Add more devices if needed
+	var device ble.Device
+	var err error
+
+	for _, dev := range devices {
+		// Extract the integer part from the device string
+		devIndexStr := strings.TrimPrefix(dev, "hci")
+		devIndex, convErr := strconv.Atoi(devIndexStr)
+		if convErr != nil {
+			continue
+		}
+
+		device, err = linux.NewDevice(ble.OptDeviceID(devIndex))
+		if err == nil {
+			return device, nil
+		}
 	}
-	return device, nil
+
+	return nil, fmt.Errorf("all devices failed")
 }
